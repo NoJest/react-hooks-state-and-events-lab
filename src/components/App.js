25ab8ae524201948abb83b9775fa2343a50b507d
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-29 15:09:17 -0400

    needed a bit of help with the filter and realizing e.target.value is going to be a very valuable pattern to learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import {useState} from "react";
 
 function App() {
+  
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // Dark mode Toggle
+  const [toggle, setToggle] = useState(false)
+  const appClass = toggle ? "App dark" : "App light"
+  const viewMode = toggle ? "Light Mode" : "Dark Mode"
+  const toggleDarkMode= () => {
+    setToggle(current => !current)
+  }
+  // Add to cart
+
+  // Filter
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick = {toggleDarkMode}>{viewMode}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {useState} from 'react';
 
 function Item({ name, category }) {
+  const [addToCart, setAddToCart] = useState(false)
+  const areYouBuying = addToCart ? "in-cart" : ""
+  const buttonText = !addToCart ? "Add to Cart" : "Remove From Cart"
+  const handleAddToCart = () => {
+    setAddToCart(current => !current)
+  }
   return (
-    <li className="">
+    <li className={areYouBuying}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {handleAddToCart}>{buttonText}</button>
     </li>
   );
 }
